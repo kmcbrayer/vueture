f9398468e63e8354d483e074e7a1c72032705d34
Kevin <kmcbrayer77@gmail.com>
2017-03-23 14:17:16 -0400

    init commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+      <div class="jumbotron text-center">
+          <img src="./assets/logo.png">
+          <router-view></router-view>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+
+</style>
